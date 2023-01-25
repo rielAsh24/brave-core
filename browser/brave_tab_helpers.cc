81fe809f9e6c9d27406ff5238944363233638e64
Taher <8665427+nullhook@users.noreply.github.com>
2023-01-25 13:07:24 -0800

    Resolved feedback

--- browser/brave_tab_helpers.cc
+++ browser/brave_tab_helpers.cc
@@ -17,7 +17,6 @@
 #include "brave/browser/ephemeral_storage/ephemeral_storage_tab_helper.h"
 #include "brave/browser/misc_metrics/page_metrics_tab_helper.h"
 #include "brave/browser/ntp_background/ntp_tab_helper.h"
-#include "brave/browser/onboarding/onboarding_tab_helper.h"
 #include "brave/browser/ui/bookmark/brave_bookmark_tab_helper.h"
 #include "brave/components/brave_perf_predictor/browser/perf_predictor_tab_helper.h"
 #include "brave/components/brave_wayback_machine/buildflags/buildflags.h"
@@ -43,6 +42,7 @@
 #endif
 
 #if !BUILDFLAG(IS_ANDROID)
+#include "brave/browser/onboarding/onboarding_tab_helper.h"
 #include "brave/browser/ui/brave_shields_data_controller.h"
 #include "brave/components/ai_chat/ai_chat_tab_helper.h"
 #include "brave/components/ai_chat/features.h"
@@ -98,7 +98,7 @@ void AttachTabHelpers(content::WebContents* web_contents) {
   // Add tab helpers here unless they are intended for android too
   BraveBookmarkTabHelper::CreateForWebContents(web_contents);
   brave_shields::BraveShieldsDataController::CreateForWebContents(web_contents);
-  onboarding::OnboardingTabHelper::CreateForWebContents(web_contents);
+  OnboardingTabHelper::MaybeCreateForWebContents(web_contents);
   ThumbnailTabHelper::CreateForWebContents(web_contents);
   if (ai_chat::features::IsAIChatEnabled()) {
     AIChatTabHelper::CreateForWebContents(web_contents);
