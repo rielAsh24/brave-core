8fb2cb4ffdb4fbeec2c9efefa5d9c96b2e7d2936
sergei <spylogsster@gmail.com>
2023-06-08 19:56:26 +0300

    wip

---
+++ components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/interactive/status_tray.cc
@@ -0,0 +1,30 @@
+#include "brave/components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/interactive/status_tray.h"
+
+#include "brave/components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/interactive/status_icon.h"
+
+StatusTray::~StatusTray() {
+}
+
+StatusIcon* StatusTray::CreateStatusIcon(
+                                         const gfx::ImageSkia& image,
+                                         const std::u16string& tool_tip) {
+  auto icon = CreatePlatformStatusIcon(type, image, tool_tip);
+  if (!icon)
+    return nullptr;
+
+  status_icons_.push_back(std::move(icon));
+  return status_icons_.back().get();
+}
+
+void StatusTray::RemoveStatusIcon(StatusIcon* icon) {
+  for (auto iter = status_icons_.begin(); iter != status_icons_.end(); ++iter) {
+    if (iter->get() == icon) {
+      status_icons_.erase(iter);
+      return;
+    }
+  }
+  NOTREACHED();
+}
+
+StatusTray::StatusTray() {
+}
---
+++ components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/interactive/status_tray.h
@@ -0,0 +1,55 @@
+#ifndef CHROME_BROWSER_STATUS_ICONS_STATUS_TRAY_H_
+#define CHROME_BROWSER_STATUS_ICONS_STATUS_TRAY_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace gfx {
+class ImageSkia;
+}
+
+class StatusIcon;
+
+// Provides a cross-platform interface to the system's status tray, and exposes
+// APIs to add/remove icons to the tray and attach context menus.
+class StatusTray {
+ public:
+  // Static factory method that is implemented separately for each platform to
+  // produce the appropriate platform-specific instance. Returns NULL if this
+  // platform does not support status icons.
+  static std::unique_ptr<StatusTray> Create();
+
+  StatusTray(const StatusTray&) = delete;
+  StatusTray& operator=(const StatusTray&) = delete;
+
+  virtual ~StatusTray();
+
+  // Creates a new StatusIcon. The StatusTray retains ownership of the
+  // StatusIcon. Returns NULL if the StatusIcon could not be created.
+  StatusIcon* CreateStatusIcon(const gfx::ImageSkia& image,
+                               const std::u16string& tool_tip);
+
+  // Removes |icon| from this status tray.
+  void RemoveStatusIcon(StatusIcon* icon);
+
+ protected:
+  using StatusIcons = std::vector<std::unique_ptr<StatusIcon>>;
+
+  StatusTray();
+
+  // Factory method for creating a status icon for this platform.
+  virtual std::unique_ptr<StatusIcon> CreatePlatformStatusIcon(
+      const gfx::ImageSkia& image,
+      const std::u16string& tool_tip) = 0;
+
+  // Returns the list of active status icons so subclasses can operate on them.
+  const StatusIcons& status_icons() const { return status_icons_; }
+
+ private:
+  // List containing all active StatusIcons. The icons are owned by this
+  // StatusTray.
+  StatusIcons status_icons_;
+};
+
+#endif  // CHROME_BROWSER_STATUS_ICONS_STATUS_TRAY_H_
