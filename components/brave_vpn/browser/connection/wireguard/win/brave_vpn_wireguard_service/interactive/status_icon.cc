3aa4f7b3baf31bd02a55b7e8de97ef3abbf68047
sergei <spylogsster@gmail.com>
2023-06-08 20:31:37 +0300

    wip

---
+++ components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/interactive/status_icon.cc
@@ -0,0 +1,53 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/interactive/status_icon.h"
+
+#include <utility>
+
+#include "base/observer_list.h"
+#include "brave/components/brave_vpn/browser/connection/wireguard/win/brave_vpn_wireguard_service/interactive/status_icon_observer.h"
+#include "build/build_config.h"
+
+StatusIcon::StatusIcon() {}
+
+StatusIcon::~StatusIcon() {}
+
+void StatusIcon::AddObserver(StatusIconObserver* observer) {
+  observers_.AddObserver(observer);
+}
+
+void StatusIcon::RemoveObserver(StatusIconObserver* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+bool StatusIcon::HasObservers() const {
+  return !observers_.empty();
+}
+
+void StatusIcon::DispatchClickEvent() {
+  for (StatusIconObserver& observer : observers_) {
+    observer.OnStatusIconClicked();
+  }
+}
+
+#if BUILDFLAG(IS_WIN)
+void StatusIcon::DispatchBalloonClickEvent() {
+  for (StatusIconObserver& observer : observers_) {
+    observer.OnBalloonClicked();
+  }
+}
+#endif
+
+void StatusIcon::ForceVisible() {}
+
+void StatusIcon::SetContextMenu() {
+  // The UI may been showing a menu for the current model, don't destroy it
+  // until we've notified the UI of the change.
+  // std::unique_ptr<StatusIconMenuModel> old_menu =
+  //  std::move(context_menu_contents_);
+  // context_menu_contents_ = std::move(menu);
+  // UpdatePlatformContextMenu(context_menu_contents_.get());
+}
