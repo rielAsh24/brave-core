57074cb742406ad2df43d9e561390e14bc970b7b
deeppandya <deeppandya91@gmail.com>
2023-06-14 16:17:49 -0400

    Add to playlist menu

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -168,6 +168,8 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
 
     private static final int PLAYLIST_MEDIA_COUNT_LIMIT = 3;
 
+    public static boolean mShouldShowPlaylistMenu = false;
+
     private PlaylistServiceObserverImpl mPlaylistServiceObserver;
 
     private DatabaseHelper mDatabaseHelper = DatabaseHelper.getInstance();
@@ -610,6 +612,7 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
     }
 
     private void hidePlaylistButton() {
+        mShouldShowPlaylistMenu = false;
         try {
             ViewGroup viewGroup =
                     BraveActivity.getBraveActivity().getWindow().getDecorView().findViewById(
@@ -644,6 +647,7 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
     }
 
     private void showPlaylistButton(PlaylistItem[] items) {
+        mShouldShowPlaylistMenu = true;
         try {
             ViewGroup viewGroup =
                     BraveActivity.getBraveActivity().getWindow().getDecorView().findViewById(
@@ -747,7 +751,7 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
         });
     }
 
-    private void addMediaToPlaylist() {
+    public void addMediaToPlaylist() {
         Tab currentTab = getToolbarDataProvider().getTab();
         if (mPlaylistService == null || currentTab == null) {
             return;
