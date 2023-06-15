07c02a177ddf671d05012c8ee14aa303c2d82d9d
Terry Mancey <terry.mancey@icloud.com>
2023-06-15 12:12:08 -0500

    Detect if Brave Ads native/system notifications are enabled on macOS before serving ads

--- browser/brave_ads/notification_helper/notification_helper_impl_mac.mm
+++ browser/brave_ads/notification_helper/notification_helper_impl_mac.mm
@@ -6,11 +6,7 @@
 #include "brave/browser/brave_ads/notification_helper/notification_helper_impl_mac.h"
 
 #import <Cocoa/Cocoa.h>
-
-// TODO(https://github.com/brave/brave-browser/issues/5541): Uncomment below
-// code when notification_platform_bridge_mac.mm has been updated to use
-// UNUserNotificationCenter
-// #import <UserNotifications/UserNotifications.h>
+#import <UserNotifications/UserNotifications.h>
 
 #include "base/feature_list.h"
 #include "base/logging.h"
@@ -22,126 +18,115 @@ namespace brave_ads {
 namespace {
 
 bool IsAuthorized() {
-  // TODO(https://github.com/brave/brave-browser/issues/5541): Uncomment below
-  // code when notification_platform_bridge_mac.mm has been updated to use
-  // UNUserNotificationCenter
+#if !defined(OFFICIAL_BUILD)
+  VLOG(0) << "Unable to detect the status of native notifications on non "
+             "official builds as the app is not code signed";
   return true;
+#else
+  // TODO(https://openradar.appspot.com/27768556): We must mock this function
+  // using NotificationHelperMock as a workaround to UNUserNotificationCenter
+  // throwing an exception during tests
 
-  // #if !defined(OFFICIAL_BUILD)
-  //   VLOG(1) << "Unable to detect the status of native notifications on non"
-  //       " official builds as the app is not code signed";
-  //   return true;
-  // #else
-  //   // TODO(https://openradar.appspot.com/27768556): We must mock this
-  //   function
-  //   // using NotificationHelperImplMock as a workaround to
-  //   UNUserNotificationCenter
-  //   // throwing an exception during tests
+  if (@available(macOS 10.14, *)) {
+    __block bool is_authorized = false;
 
-  //   if (@available(macOS 10.14, *)) {
-  //     __block bool is_authorized = false;
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
 
-  //     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    UNUserNotificationCenter* notification_center =
+        [UNUserNotificationCenter currentNotificationCenter];
 
-  //     UNUserNotificationCenter *notificationCenter =
-  //         [UNUserNotificationCenter currentNotificationCenter];
+    [notification_center
+        requestAuthorizationWithOptions:UNAuthorizationOptionAlert |
+                                        UNAuthorizationOptionBadge |
+                                        UNAuthorizationOptionSound
+                      completionHandler:^(BOOL granted,
+                                          NSError* _Nullable error) {
+                        if (granted) {
+                          VLOG(0) << "User granted authorization to show "
+                                     "notifications";
+                        } else {
+                          VLOG(0) << "User denied authorization to show "
+                                     "notifications";
+                        }
 
-  //     [notificationCenter
-  //     requestAuthorizationWithOptions:UNAuthorizationOptionAlert
-  //         completionHandler:^(BOOL granted, NSError * _Nullable error) {
-  //       if (granted) {
-  //         VLOG(1) << "User granted authorization to show notifications";
-  //       } else {
-  //         VLOG(1) << "User denied authorization to show notifications";
-  //       }
+                        is_authorized = granted;
 
-  //       is_authorized = granted;
+                        dispatch_semaphore_signal(semaphore);
+                      }];
 
-  //       dispatch_semaphore_signal(semaphore);
-  //     }];
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    dispatch_release(semaphore);
 
-  //     dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-  //     dispatch_release(semaphore);
+    return is_authorized;
+  }
 
-  //     if (!is_authorized) {
-  //       VLOG(1) << "User is not authorized to show notifications";
-  //     }
-
-  //     return is_authorized;
-  //   }
-
-  //   return true;
-  // #endif
+  return true;
+#endif
 }
 
 bool IsEnabled() {
-  // TODO(https://github.com/brave/brave-browser/issues/5541): Uncomment below
-  // code when notification_platform_bridge_mac.mm has been updated to use
-  // UNUserNotificationCenter
+#if !defined(OFFICIAL_BUILD)
+  VLOG(0) << "Unable to detect the status of native notifications on non "
+             "official builds as the app is not code signed";
   return true;
+#else
+  // TODO(https://openradar.appspot.com/27768556): We must mock this function
+  // using NotificationHelperMock as a workaround to UNUserNotificationCenter
+  // throwing an exception during tests
 
-  // #if !defined(OFFICIAL_BUILD)
-  //   VLOG(1) << "Unable to detect the status of native notifications on non"
-  //       " official builds as the app is not code signed";
-  //   return true;
-  // #else
-  //   // TODO(https://openradar.appspot.com/27768556): We must mock this
-  //   function
-  //   // using NotificationHelperImplMock as a workaround to
-  //   UNUserNotificationCenter
-  //   // throwing an exception during tests
+  if (@available(macOS 10.14, *)) {
+    __block bool is_authorized = false;
 
-  //   if (@available(macOS 10.14, *)) {
-  //     __block bool is_authorized = false;
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
 
-  //     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    UNUserNotificationCenter* notification_center =
+        [UNUserNotificationCenter currentNotificationCenter];
 
-  //     UNUserNotificationCenter *notificationCenter =
-  //         [UNUserNotificationCenter currentNotificationCenter];
+    [notification_center getNotificationSettingsWithCompletionHandler:^(
+                             UNNotificationSettings* _Nonnull settings) {
+      switch (settings.authorizationStatus) {
+        case UNAuthorizationStatusDenied: {
+          VLOG(0) << "Notification authorization status denied";
+          is_authorized = false;
+          break;
+        }
 
-  //     [notificationCenter getNotificationSettingsWithCompletionHandler:
-  //         ^(UNNotificationSettings * _Nonnull settings) {
-  //       switch (settings.authorizationStatus) {
-  //         case UNAuthorizationStatusDenied: {
-  //           VLOG(1) << "Notification authorization status denied";
-  //           is_authorized = false;
-  //           break;
-  //         }
+        case UNAuthorizationStatusNotDetermined: {
+          VLOG(0) << "Notification authorization status not determined";
+          is_authorized = true;
+          break;
+        }
 
-  //         case UNAuthorizationStatusNotDetermined: {
-  //           VLOG(1) << "Notification authorization status not determined";
-  //           is_authorized = true;
-  //           break;
-  //         }
+        case UNAuthorizationStatusAuthorized: {
+          VLOG(0) << "Notification authorization status authorized";
+          is_authorized = true;
+          break;
+        }
 
-  //         case UNAuthorizationStatusAuthorized: {
-  //           VLOG(1) << "Notification authorization status authorized";
-  //           is_authorized = true;
-  //           break;
-  //         }
+        case UNAuthorizationStatusProvisional: {
+          VLOG(0) << "Notification authorization status provisional";
+          is_authorized = true;
+          break;
+        }
 
-  //         case UNAuthorizationStatusProvisional: {
-  //           VLOG(1) << "Notification authorization status provisional";
-  //           is_authorized = true;
-  //           break;
-  //         }
-  //       }
+        default: {
+          VLOG(0) << "Notification authorization status unknown";
+          is_authorized = true;
+          break;
+        }
+      }
 
-  //       dispatch_semaphore_signal(semaphore);
-  //     }];
+      dispatch_semaphore_signal(semaphore);
+    }];
 
-  //     dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-  //     dispatch_release(semaphore);
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    dispatch_release(semaphore);
 
-  //     if (!is_authorized) {
-  //       VLOG(1) << "Notifications not authorized";
-  //     }
+    return is_authorized;
+  }
 
-  //     return is_authorized;
-  //   }
-
-  //   return true;
-  // #endif
+  return true;
+#endif
 }
 
 }  // namespace
@@ -152,20 +137,22 @@ NotificationHelperImplMac::~NotificationHelperImplMac() = default;
 
 bool NotificationHelperImplMac::CanShowNotifications() {
   if (!base::FeatureList::IsEnabled(::features::kNativeNotifications)) {
-    VLOG(1) << "Native notifications feature is disabled";
+    VLOG(0) << "Native notifications feature is disabled";
     return false;
   }
 
   if (base::mac::IsAtMostOS10_13()) {
-    VLOG(1) << "Native notifications are not supported prior to macOS 10.14";
-    return false;
-  }
-
-  if (!IsAuthorized()) {
+    VLOG(0) << "Native notifications are not supported prior to macOS 10.14";
     return false;
   }
 
   if (!IsEnabled()) {
+    VLOG(0) << "Native notifications are not enabled";
+    return false;
+  }
+
+  if (!IsAuthorized()) {
+    VLOG(0) << "Native notifications are not authorized";
     return false;
   }
 
